90d76097449ca8cd4534e41cc398f9ac1c1447ad
Aleksei Prisukhin <prisukhin@gmail.com>
2016-07-17 14:29:11 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Function swap color for logotype at menu
+
+function menuChangeColorOn() {
+	document.getElementById('menuLogoText').style.color = '#4b71f0';
+	document.getElementById('menuLogo').style.color = '#fff';
+}
+
+function menuChangeColorOff() {
+	document.getElementById('menuLogoText').style.color = '#fff';
+	document.getElementById('menuLogo').style.color = '#4b71f0';
+}
